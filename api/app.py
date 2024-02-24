7144b4f7c403996cfd9b54f1fc17aec98d7b247a
Ibrahim8879 <ibrahimqayyum7891234@gmail.com>
2024-02-24 14:44:15 +0500

    Working db.

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ from flask_apscheduler import APScheduler
 from schedular_db_push import collect_data_job
 import os
 import csv
+import subprocess
 
 app = Flask(__name__)
 scheduler = APScheduler()
@@ -15,29 +16,22 @@ basedir = basedir + '\\db\\twitter.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///twitter.db'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + basedir
-<<<<<<< Updated upstream
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-class TweetsAttribute(db.Model):
-    __tablename__ = 'tweets_attribute'
-=======
-db = SQLAlchemy(app)
-
 class ProfileData(db.Model):
->>>>>>> Stashed changes
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
     display_name = db.Column(db.String(255))
     birth_date = db.Column(db.String(255))
     description = db.Column(db.String(500))
     location = db.Column(db.String(255))
-    followers = db.Column(db.String(100))
-    following = db.Column(db.String(100))
-    verified = db.Column(db.String(100))
-    num_posts = db.Column(db.String(100))
+    followers = db.Column(db.String(50))
+    following = db.Column(db.String(50))
+    verified = db.Column(db.String(50))
+    num_posts = db.Column(db.String(50))
     date_joined = db.Column(db.String(255))
-class Tweet(db.Model):
+class Tweets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String(255))
     country = db.Column(db.String(255))
@@ -50,44 +44,66 @@ class Tweet(db.Model):
 #scheduling the job
 def job():
     with app.app_context():
-        print("Scheduled job executed")
+        # Scrapper Running here.
+        #exe_path = os.path.abspath(os.getcwd())
+        #exe_path = exe_path+'\\api\\data_collection\\twitter_scraper_main.exe'
+        #subprocess.call([exe_path])
+        print("Scheduled job executed.")
         # Function to push data from CSV files to the database
+        file_path1 = datacoll+'\\api\\data_collection\\ProfileData.csv'
+        file_path2 = datacoll+'\\api\data_collection\\tweets.csv'
         db.create_all()
-        with open(datacoll+'\\api\\data_collection\\ProfileData.csv', 'r', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                profile_data = ProfileData(
-                    username=row['Username'],
-                    display_name=row['Display Name'],
-                    birth_date=row['Birth Date'],
-                    description=row['Description'],
-                    location=row['Location'],
-                    followers=row['Followers'],
-                    following=row['Following'],
-                    verified=row['Verified'],
-                    num_posts=row['Number of Posts'],
-                    date_joined=row['Date Joined']
-                )
-                db.session.add(profile_data)
-        with open(datacoll+'\\api\data_collection\\tweets.csv', 'r', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                tweet = Tweet(
-                    date=row['Date'],
-                    country=row['Country'],
-                    trend=row['Trend'],
-                    username=row['Username'],
-                    tweet=row['Tweet'],
-                    language=row['Language'],
-                    tweet_time=row['Tweet_Time']
-                )
-                db.session.add(tweet)
-        db.session.commit()
-        print("Data pushed to database")
+        with open(file_path2, 'r', encoding='utf-8') as file:
+            line_count = sum(1 for line in file)
+            if line_count > 2:
+                with open(datacoll+'\\api\\data_collection\\ProfileData.csv', 'r', encoding='utf-8') as file:
+                    reader = csv.DictReader(file)
+                    for row in reader:
+                        profile_data = ProfileData(
+                            username=row['Username'],
+                            display_name=row['Display Name'],
+                            birth_date=row['Birth Date'],
+                            description=row['Description'],
+                            location=row['Location'],
+                            followers=row['Followers'],
+                            following=row['Following'],
+                            verified=row['Verified'],
+                            num_posts=row['Number of Posts'],
+                            date_joined=row['Date Joined']
+                        )
+                        db.session.add(profile_data)
+                with open(datacoll+'\\api\data_collection\\tweets.csv', 'r', encoding='utf-8') as file:
+                    reader = csv.DictReader(file)
+                    for row in reader:
+                        tweet = Tweets(
+                            date=row['Date'],
+                            country=row['Country'],
+                            trend=row['Trend'],
+                            username=row['Username'],
+                            tweet=row['Tweet'],
+                            language=row['Language'],
+                            tweet_time=row['Tweet_Time']
+                        )
+                        db.session.add(tweet)
+                db.session.commit()
+                print("Data pushed to database.")
+            else:
+                print("File Not Updated. So, dont update db.")
+
+        #cleaning data files
+        def clean_file(file_path):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                first_line = file.readline()  # Read the first line
+            # Rewrite the file with just the first line
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(first_line)
+        # Example usage
+        clean_file(file_path1)
+        clean_file(file_path2)
+        print("Files are also cleaned.")
 
 # Schedule the job to run every 12 hours
-scheduler.add_job(id='collect_data_job', func=job, trigger='interval', seconds=5)
-
+scheduler.add_job(id='collect_data_job', func=job, trigger='interval', seconds=15)
 
 
 @app.errorhandler(404)
@@ -102,8 +118,8 @@ def home():
     
 if __name__ == '__main__':
     if os.environ.get('PORT') is not None:
-        scheduler.start()
+        #scheduler.start()
         app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT'))
     else:
-        scheduler.start()
+        #scheduler.start()
         app.run(debug=True, host='0.0.0.0')
